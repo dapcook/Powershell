fe8f852add75f56583818b28e2796449a78fb7d4
Dave Cook <dapcook@gmail.com>
2014-02-17 15:42:52 -0600

    Create CheckMySites.ps1

---
+++ CheckMySites.ps1
@@ -0,0 +1,57 @@
+$smtpServer = "mail.kumed.com"
+$servers = "www.kumed.com", "www.kucancercenter.org", "dcook2-web"
+$collection = $()
+$SendEmail = $false
+
+$web = New-Object Net.WebClient
+$tcp = New-Object Net.Sockets.TcpClient
+$msg = New-Object Net.Mail.MailMessage
+$smtp = New-Object Net.Mail.SmtpClient($smtpServer)
+
+$msg.From = "noreply@kumc.edu"
+$msg.ReplyTo = "noreply@kumc.edu"
+$msg.To.Add("dcook2@kumc.edu")
+$msg.Subject = "CheckMySites Notification"
+
+foreach($server in $servers)
+{ 
+    $status = @{ "ServerName" = $server; "TimeStamp" = (Get-Date -f s) }
+    if( Test-Connection $server -Count 1 -ea 0 -Quiet)
+    {
+        $status["Results"] = "Up"
+
+        try{
+            $tcp.Connect($server, 80)
+        }
+        catch{}
+
+        if( $tcp.Connected ){
+            $status["Port80 Ok"] = "yes"
+            $url = "http://" + $server
+            $SiteContent = $web.DownloadString($url)
+            $status["ContentLength"] = $SiteContent.Length.ToString()
+        }else{
+            $status["Port80 Ok"] = "no"
+            $status["contentLength"] = 0
+            $SendEmail = $true
+        }
+        $tcp = New-Object Net.Sockets.TcpClient
+    }
+    else
+    {
+        $status["Results"] = "Down"
+        $SendEmail = $true
+    }
+    New-Object -TypeName PSObject -Property $status -OutVariable serverStatus
+    $collection += $serverStatus
+}
+if($SendEmail)
+{
+    $msg.Body = $collection | Out-String
+    $smtp.Send($msg)
+}
+else{
+    $collection | Export-Csv -LiteralPath .\ServerStatus.csv -NoTypeInformation
+}
+
+
